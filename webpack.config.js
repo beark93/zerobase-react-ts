16eb5078bc6fd0c43570d095386e1670a2289b4b
beark93 <beark93@gmail.com>
2023-10-13 09:51:01 +0900

    install react / ts / webpack / babel

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import TsconfigPathsPlugin from "tsconfig-paths-webpack-plugin";
+import { CleanWebpackPlugin } from "clean-webpack-plugin";
+
+const __dirname = path.resolve();
+const tsConfigPath = path.resolve(__dirname, 'tsconfig.json');
+
+const production = (process.env.NODE_ENV === 'production');
+
+export default function config(env, argv) {
+  return {
+    devtool: production ? 'source-map' : 'cheap-module-source-map',
+    mode: production ? 'production' : 'development',
+    entry: path.join(__dirname, 'src/index.tsx'),
+    devServer: {
+      historyApiFallback: true,
+      port: 3000,
+      hot: true,
+      static: path.join(__dirname, 'dist'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx|js|jsx)$/,
+          use: 'babel-loader',
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/,
+          use: ['file-loader'],
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ]
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.join(__dirname, 'dist'),
+    },
+    resolve: {
+      modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+      extensions: ['.ts', '.tsx', '.js', '.json'],
+      plugins: [
+        new TsconfigPathsPlugin({
+          configFile: tsConfigPath,
+        }),
+      ],
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './public/index.html',
+      }),
+      new CleanWebpackPlugin(),
+    ]
+  }
+}
